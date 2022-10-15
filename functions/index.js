8504a8439347b27928269e007e052e45fd81f926
Aashish Panthi <aashishpanthi11@gmail.com>
2022-10-15 09:54:10 +0545

    test function variables

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,25 @@
+import { NhostClient } from "@nhost/nhost-js";
+
+const nhost = new NhostClient({
+  backendUrl: process.env.NHOST_BACKEND_URL,
+});
+
+nhost.functions.setAccessToken(process.env.NHOST_ADMIN_SECRET);
+
 export default (req, res) => {
-  res.status(200).send(`Hello Wrold!`);
+  // get the data from the request
+  const imgText = req.query.text || "";
+
+  // get the email id from the request
+
+  // update query using the email id
+
+  //update the seen column in emails table
+
+  // return the updated data
+  res.status(200).send({
+    imgText,
+    backendUrl: process.env.NHOST_BACKEND_URL,
+    adminSecret: process.env.NHOST_ADMIN_SECRET,
+  });
 };
