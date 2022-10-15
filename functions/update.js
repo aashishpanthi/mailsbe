94c8983a062d74ab8b30b7c009b95dde20216c4d
Aashish Panthi <aashishpanthi11@gmail.com>
2022-10-15 10:57:23 +0545

    solve the promise bug

--- functions/update.js
+++ functions/update.js
@@ -20,43 +20,37 @@ export default async (req, res) => {
     }
   }`;
 
-  // get the email id from the request
-  const { data, error } = await nhost.graphql.request(GET_EMAIL_ID, {
-    text: imgText,
-  });
+  // update query with the email id
+  const UPDATE_QUERY = `
+    mutation UpdateEmail($id: Int, $date: timestamptz) {
+      update_emails(where: {id: {_eq: $id}}, _set: {seen: true, seen_at: $date}) {
+        affected_rows
+      }
+    }`;
 
-  if (error) {
+  try {
+    const { data } = await nhost.graphql.query(GET_EMAIL_ID, {
+      variables: {
+        text: imgText,
+      },
+    });
+
+    // extract the email id from the response
+    const emailId = data.emails[0].id;
+
+    //update the seen column in emails table
+    const { data: updatedData } = await nhost.graphql.request(UPDATE_QUERY, {
+      id: emailId,
+      date: new Date().toString(),
+    });
+
+    // return the updated data
+    res.status(200).send({
+      imgText,
+      data,
+      updatedData,
+    });
+  } catch (error) {
     res.status(500).json({ error });
   }
-
-  // extract the email id from the response
-  const emailId = data.emails[0].id;
-
-  // update query using the email id
-  const UPDATE_QUERY = `
-  mutation UpdateEmail($id: Int, $date: timestamptz) {
-    update_emails(where: {id: {_eq: $id}}, _set: {seen: true, seen_at: $date}) {
-      affected_rows
-    }
-  }`;
-
-  //update the seen column in emails table
-  const { data: updatedData, error: updateError } = await nhost.graphql.request(
-    UPDATE_QUERY,
-    {
-      id: emailId,
-      date: new Date(),
-    }
-  );
-
-  if (error) {
-    res.status(500).json({ error: updateError });
-  }
-
-  // return the updated data
-  res.status(200).send({
-    imgText,
-    data,
-    updatedData,
-  });
 };
